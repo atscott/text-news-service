68f3c418ed0bd87a8c236f9125c53b7ab9d54e2b
Andrew Scott <atscott01@gmail.com>
2014-02-11 16:59:26 -0600

    login page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-var controllers = angular.module('myApp.controllers');
+var controllers = angular.module('myApp.controllers',[]);
 
 controllers.controller('MyCtrl1', [function () {
 
@@ -12,6 +12,26 @@ controllers.controller('MyCtrl2', [function () {
 
 }]);
 
-controllers.controller('LoginCtrl', [function(){
+controllers.controller('LoginCtrl', ['$scope', '$location', 'Authentication', function ($scope, $location, Authentication) {
+  $scope.submit = function () {
+    Authentication.login($scope.user.username, $scope.user.password).then(function (response) {
+      if (response.status != 200) {
+        var message = response.data.Message;
+        if (message == null || message.length < 1) {
+          message = "Something went wrong (Error code " + response.status + ")";
+        }
+        $scope.showAlert(message);
+      } else {
+        $location.path('/view1');
+      }
+    });
+  };
 
+  $scope.showAlert = function (message) {
+    $scope.alert = {msg: message};
+  };
+
+  $scope.closeAlert = function () {
+    $scope.alert = null;
+  };
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,28 @@
 
 /* Services */
 
+ var user1 = {username: 'Andrew', password: 'Scott'};
+  var user2 = {username: 'user', password: 'user'};
+  var users = [user1, user2];
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
 var services = angular.module('myApp.services', []);
 services.value('version', '0.1');
+
+
+services.factory('Authentication', ['$q', function ($q) {
+  return{
+    login: function (username, password) {
+      var deferred = $q.defer();
+      var valid = false;
+      $.each(users, function () {
+        if (this.username == username && this.password == password) {
+          deferred.resolve({status:200})
+        }
+      });
+      deferred.resolve({data:{Message: 'invalid credentials'}, status:400});
+      return deferred.promise;
+    }
+  }
+}]);
